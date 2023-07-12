fc366cac1227594751314d4ca044202bad6e91d5
Lauren Miller <lmiller90@fordham.edu>
2023-07-12 17:08:41 -0400

    finished writing out product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,42 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      auto_increment: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          msg: 'price must be a valid decimal number'
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: {
+          msg: 'stock must be a valid numeric value'
+        }
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
